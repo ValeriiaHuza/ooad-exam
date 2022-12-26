e92a3e6504d28107582d55a62682312fee16d6d7
Марія-Валерія Гуза <mariia-valeriia.huza@ukma.edu.ua>
2022-12-26 23:00:03 +0200

    First commit

---
+++ src/main/java/com/example/ooad/OoadExamHuzaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ooad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OoadExamHuzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OoadExamHuzaApplication.class, args);
+	}
+
+}
